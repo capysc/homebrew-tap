080b3d1f587f239f564b98a2ceecbcd9f38d017a
cvince <vincent@vincentchan.vc>
2026-05-22 12:34:16 -0700

    capy 0.5.0

--- Formula/capy.rb
+++ Formula/capy.rb
@@ -1,8 +1,8 @@
 class Capy < Formula
   desc "End-to-end encrypted environment variables for your team"
   homepage "https://docs.capy.sc"
-  url "https://registry.npmjs.org/@capysc/cli/-/cli-0.4.1.tgz"
-  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  url "https://registry.npmjs.org/@capysc/cli/-/cli-0.5.0.tgz"
+  sha256 "ece4056e9866c14f89886c58564c7e49fb73e94e20c926ec9f2399e37daa918e"
   license "AGPL-3.0-only"
 
   depends_on "node"
